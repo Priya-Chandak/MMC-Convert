7bbaebc00b7a332437017b1743a19d917fc491d3
Priya Chandak <priyachandak100@gmail.com>
2022-05-17 14:49:39 +0530

    Updated Code

--- get_invoice.py
+++ get_invoice.py
@@ -1,38 +1,86 @@
 import json
+from ast import Break
+import json
+from os import path
+from os.path import exists
 import requests
-from static import *
+from static import payload, headers
 
 
 url = "https://arl2.api.myob.com/accountright/53f60d69-ae83-4722-99a8-bdfc30d65040/Sale/Invoice/Item"
-
-
 response = requests.request("GET", url, headers=headers, data=payload)
-
 a = response.json()
 
-arr = []
-for i in range(0, len(a['Items'])):
-    e = {}
-    e['UID'] = a['Items'][i]["UID"]
-    e['TotalAmount'] = a['Items'][i]['TotalAmount']
-    e['Customer_Name'] = a['Items'][i]['Customer']['Name']
-    e['Invoice_Number'] = a['Items'][i]['Number']
-    e['Date'] = a['Items'][i]['Date']
-    e['Due_Date'] = a['Items'][i]['Terms']['DueDate']
+n = a['Count']
+count = 0
 
-    for j in range(0, len(a['Items'][i]['Lines'])):
-        e['Description'] = a['Items'][i]['Lines'][j]['Description']
-        e['Unit_Price'] = a['Items'][i]['Lines'][j]['UnitPrice']
-        e['Total'] = a['Items'][i]['Lines'][j]['Total']
-        e['Unit_Count'] = a['Items'][i]['Lines'][j]['UnitCount']
-        e['Item_Name'] = a['Items'][i]['Lines'][j]['Item']['Name']
+while n != 0:
+    n //= 10
+    count += 1
 
-    arr.append(e)
+if count == 4:
+    limit = 1000
+elif count == 3:
+    limit = 100
+else:
+    limit = 10
+
+print("Number of digits: " + str(count))
+print(limit)
+
+url = "https://arl2.api.myob.com/accountright/53f60d69-ae83-4722-99a8-bdfc30d65040/Sale/Invoice/Item/?$top={}".format(
+    limit)
+response = requests.request("GET", url, headers=headers, data=payload)
+a = response.json()
+path = input("Enter a Path:")
 
 
-path = input("Enter a path : ")
-jsonString = json.dumps(arr)
-jsonFile = open(
-    "{}/invoice.json".format(path), "w")
-jsonFile.write(jsonString)
-jsonFile.close()
+def get_data(url):
+    response = requests.request("GET", url, headers=headers, data=payload)
+    a = response.json()
+
+    arr = []
+    for i in range(0, len(a['Items'])):
+        e = {}
+        e['UID'] = a['Items'][i]["UID"]
+        e['TotalAmount'] = a['Items'][i]['TotalAmount']
+        e['Customer_Name'] = a['Items'][i]['Customer']['Name']
+        e['Invoice_Number'] = a['Items'][i]['Number']
+        e['Date'] = a['Items'][i]['Date']
+        e['Due_Date'] = a['Items'][i]['Terms']['DueDate']
+
+        for j in range(0, len(a['Items'][i]['Lines'])):
+            e['Description'] = a['Items'][i]['Lines'][j]['Description']
+            e['Unit_Price'] = a['Items'][i]['Lines'][j]['UnitPrice']
+            e['Total'] = a['Items'][i]['Lines'][j]['Total']
+            e['Unit_Count'] = a['Items'][i]['Lines'][j]['UnitCount']
+            e['Item_Name'] = a['Items'][i]['Lines'][j]['Item']['Name']
+
+        arr.append(e)
+
+        filename = "{}/invoice.json".format(path)
+        file_exists = exists("{}/invoice.json".format(path))
+
+        if file_exists == False:
+            with open("{}/invoice.json".format(path), "w") as jsonFile:
+                jsonString = json.dumps(arr)
+                jsonFile.write(jsonString)
+                jsonFile.close()
+            print("The json file is created")
+        else:
+            with open(filename) as fp:
+                listObj = json.load(fp)
+                listObj.append(e)
+                with open(filename, 'w') as json_file:
+                    json.dump(listObj, json_file, separators=(',', ': '))
+
+    if a['NextPageLink'] != None:
+        print(a['NextPageLink'])
+        get_data(a['NextPageLink'])
+
+    else:
+        print("Data Over")
+        Break
+
+
+get_data(url)
