18fa3554aff803447a5ff4a5bb5a5a19d1dc0052
Priya Chandak <priyachandak100@gmail.com>
2022-05-17 13:39:56 +0530

    Added Pagination Functionality

--- get_item.py
+++ get_item.py
@@ -1,34 +1,83 @@
-import json
 import requests
-from static import *
+import json
+from ast import Break
+import json
+from os import path
+from os.path import exists
+from static import payload, headers
 
 
 url = "https://arl2.api.myob.com/accountright/53f60d69-ae83-4722-99a8-bdfc30d65040/Sale/Invoice/Item"
-
-
 response = requests.request("GET", url, headers=headers, data=payload)
-
-
-print(response.text)
-
 a = response.json()
 
-arr = []
-for i in range(0, len(a['Items'])):
-    e = {}
-    e['UID'] = a['Items'][i]["UID"]
-    e['TotalAmount'] = a['Items'][i]['TotalAmount']
-    e['Customer_Name'] = a['Items'][i]['Customer']['Name']
-    e['DiscountDate'] = a['Items'][i]['Terms']['DiscountDate']
-    e['BalanceDueDate'] = a['Items'][i]['Terms']['BalanceDueDate']
+n = a['Count']
+count = 0
 
-    for j in range(0, len(a['Items'][i]['Lines'])):
-        e['Description'] = a['Items'][i]['Lines'][j]['Description']
+while n != 0:
+    n //= 10
+    count += 1
 
-    arr.append(e)
+if count == 4:
+    limit = 1000
+elif count == 3:
+    limit = 100
+else:
+    limit = 10
 
-path = input("Enter a path : ")
-jsonString = json.dumps(arr)
-jsonFile = open("{}/item.json".format(path), "w")
-jsonFile.write(jsonString)
-jsonFile.close()
+print("Number of digits: " + str(count))
+print(limit)
+
+url = "https://arl2.api.myob.com/accountright/53f60d69-ae83-4722-99a8-bdfc30d65040/Sale/Invoice/Item/?$top={}".format(
+    limit)
+response = requests.request("GET", url, headers=headers, data=payload)
+a = response.json()
+
+path=input("Enter a Path where you want to store the data file:")
+    
+
+def get_data(url):
+    response = requests.request("GET", url, headers=headers, data=payload)
+    a = response.json()
+
+    arr = []
+    for i in range(0, len(a['Items'])):
+        e = {}
+        e['UID'] = a['Items'][i]["UID"]
+        e['TotalAmount'] = a['Items'][i]['TotalAmount']
+        e['Customer_Name'] = a['Items'][i]['Customer']['Name']
+        e['DiscountDate'] = a['Items'][i]['Terms']['DiscountDate']
+        e['BalanceDueDate'] = a['Items'][i]['Terms']['BalanceDueDate']
+
+        for j in range(0, len(a['Items'][i]['Lines'])):
+            e['Description'] = a['Items'][i]['Lines'][j]['Description']
+
+        arr.append(e)
+
+        
+        filename = "{}/item.json".format(path)
+        file_exists = exists("{}/demo.json".format(path))
+
+        if file_exists == False:
+            with open("{}/item.json".format(path), "w") as jsonFile:
+                jsonString = json.dumps(arr)
+                jsonFile.write(jsonString)
+                jsonFile.close()
+            print("The json file is created")
+        else:
+            with open(filename) as fp:
+                listObj = json.load(fp)
+                listObj.append(e)
+                with open(filename, 'w') as json_file:
+                    json.dump(listObj, json_file, separators=(',', ': '))
+
+    if a['NextPageLink'] != None:
+        print(a['NextPageLink'])
+        get_data(a['NextPageLink'])
+
+    else:
+        print("No More data to GET")
+        Break
+
+
+get_data(url)
