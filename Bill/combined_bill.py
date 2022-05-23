95495cd4d66cea600d22051cdd49fb4a7d03a682
Priya Chandak <priyachandak100@gmail.com>
2022-05-23 16:25:28 +0530

    Updated Bill

--- Bill/combined_bill.py
+++ Bill/combined_bill.py
@@ -1,25 +1,38 @@
 import json
 
-path1 = input("Enter a path of file-1: ")
-path2 = input("Enter a path of file-2: ")
-
+path1 = input("Enter a path of file: ")
 
 with open('{}/item_bill.json'.format(path1), 'r') as f:
     data1 = json.load(f)
 
-with open('{}/all_bill.json'.format(path2), 'r') as f:
+with open('{}/service_bill.json'.format(path1), 'r') as f:
     data2 = json.load(f)
 
+with open('{}/professional_bill.json'.format(path1), 'r') as f:
+    data3 = json.load(f)
 
-for i in range(0,len(data1)):
-    
-    if data1[i]['UID'] == data2[i]['UID']:
-        data2[i].update(data1[i])
-    else:
-        pass
+with open('{}/misc_bill.json'.format(path1), 'r') as f:
+    data4 = json.load(f)
 
-with open("{}/combined_bill.json".format(path1), "w") as jsonFile:
-    jsonString = json.dumps(data2)
-    jsonFile.write(jsonString)
-    jsonFile.close()
-print("The json file is created")
+with open('{}/all_bill.json'.format(path1), 'r') as f:
+    data5 = json.load(f)
+
+def get_data(data1):
+    for i in range(0,len(data1)):
+        for j in range(0,len(data5)):
+            if data1[i]['UID'] == data5[j]['UID']:
+                data5[j].update(data1[i])
+            else:
+                pass
+            
+    with open("{}/combined_bill.json".format(path1), "w") as jsonFile:
+        jsonString = json.dumps(data5)
+        jsonFile.write(jsonString)
+        jsonFile.close()
+    print("The json file is created")
+
+
+get_data(data1)
+get_data(data2)
+get_data(data3)
+get_data(data4)
