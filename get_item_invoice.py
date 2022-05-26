dd4f33682f976a1eaafa9b3ec9d52ab0a38be9c4
Priya Chandak <priyachandak100@gmail.com>
2022-05-26 10:39:15 +0530

    AAdded code for Item

--- get_item_invoice.py
+++ get_item_invoice.py
@@ -1,13 +1,13 @@
+import requests
 import json
 from ast import Break
 import json
 from os import path
 from os.path import exists
-import requests
 from static import payload, headers
 
 
-url = "https://api.myob.com/accountright/53f60d69-ae83-4722-99a8-bdfc30d65040/Inventory/Item"
+url = "https://arl2.api.myob.com/accountright/53f60d69-ae83-4722-99a8-bdfc30d65040/Sale/Invoice/Item"
 response = requests.request("GET", url, headers=headers, data=payload)
 a = response.json()
 
@@ -18,22 +18,20 @@ while n != 0:
     n //= 10
     count += 1
 
-if count >= 4:
+if count == 4:
     limit = 1000
 elif count == 3:
     limit = 100
 else:
     limit = 10
 
-print("Number of digits: " + str(count))
-print(limit)
-
-url = "https://api.myob.com/accountright/53f60d69-ae83-4722-99a8-bdfc30d65040/Inventory/Item/?$top={}".format(
+url = "https://arl2.api.myob.com/accountright/53f60d69-ae83-4722-99a8-bdfc30d65040/Sale/Invoice/Item/?$top={}".format(
     limit)
 response = requests.request("GET", url, headers=headers, data=payload)
 a = response.json()
-path = input("Enter a Path:")
 
+path=input("Enter a Path where you want to store the data file:")
+    
 
 def get_data(url):
     response = requests.request("GET", url, headers=headers, data=payload)
@@ -42,30 +40,20 @@ def get_data(url):
     arr = []
     for i in range(0, len(a['Items'])):
         e = {}
-        e['UID'] = a['Items'][i]['UID']
-        e['Description'] = a['Items'][i]['Description']
-        e['Number'] = a['Items'][i]['Number']
-        e['Name'] = a['Items'][i]['Name']
-        e['ExpenseAccount'] = a['Items'][i]['ExpenseAccount']
-        e['Income_Account_ID'] = a['Items'][i]['IncomeAccount']['UID']
-        e['Income_Account_Name'] = a['Items'][i]['IncomeAccount']['Name']
-        e['Is_Tax_Inclusive'] = a['Items'][i]['SellingDetails']['IsTaxInclusive']
-        
-        if a['Items'][i]['AssetAccount'] != None:
-            e['Asset_Account_ID'] = a['Items'][i]['AssetAccount']['UID']
-            e['Asset_Account_Name'] = a['Items'][i]['AssetAccount']['Name']
-        else:
-            pass
-        
-        if a['Items'][i]['CostOfSalesAccount'] != None:
-            e['Expense_Account_ID'] = a['Items'][i]['CostOfSalesAccount']['UID']
-            e['Expense_Account_Name'] = a['Items'][i]['CostOfSalesAccount']['Name']
+        e['UID'] = a['Items'][i]["UID"]
+        e['TotalAmount'] = a['Items'][i]['TotalAmount']
+        e['Customer_Name'] = a['Items'][i]['Customer']['Name']
+        e['DiscountDate'] = a['Items'][i]['Terms']['DiscountDate']
+        e['BalanceDueDate'] = a['Items'][i]['Terms']['BalanceDueDate']
 
+        for j in range(0, len(a['Items'][i]['Lines'])):
+            e['Description'] = a['Items'][i]['Lines'][j]['Description']
 
         arr.append(e)
 
+        
         filename = "{}/item.json".format(path)
-        file_exists = exists("{}/item.json".format(path))
+        file_exists = exists("{}/demo.json".format(path))
 
         if file_exists == False:
             with open("{}/item.json".format(path), "w") as jsonFile:
@@ -85,9 +73,8 @@ def get_data(url):
         get_data(a['NextPageLink'])
 
     else:
-        print("Data Over")
+        print("No More data to GET")
         Break
 
 
 get_data(url)
-
